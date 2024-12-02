6e32f6ab016de8a81a484fa4d5ee912682a3fae4
Kermabon Stéphane <demis27@demis27.net>
2024-12-02 11:11:10 +0100

    Fix some sonar issue

--- src/main/java/org/demis27/lefacteur/environment/Environment.java
+++ src/main/java/org/demis27/lefacteur/environment/Environment.java
@@ -1,37 +1,24 @@
 package org.demis27.lefacteur.environment;
 
 import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
-import java.util.function.Function;
 
-@Getter
-@Setter
 @Builder(toBuilder = true)
-public class Environment {
-
-    private UUID id;
-
-    private String name;
-
-    private List<Element> elements;
+public record Environment(
+        UUID id,
+        String name,
+        List<Element> elements) {
 
     public Optional<Environment.Element> getElement(String key) {
         return elements.stream().filter(element -> element.key.equals(key)).findFirst();
     }
 
-    @Getter
-    @Setter
     @Builder(toBuilder = true)
-    public static class Element {
-
-        private String key;
-
-        private String value;
-
+    public static record Element(
+            String key,
+            String value) {
     }
 }
